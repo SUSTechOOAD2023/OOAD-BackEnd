7357176e7b84b7a3647085f8b355cb2e244fdfc7
SendingA <12110813@mail.sustech.edu.cn>
2023-12-21 22:33:37 +0800

    update info

--- src/main/java/com/example/controller/StudentController.java
+++ src/main/java/com/example/controller/StudentController.java
@@ -29,12 +29,19 @@ public class StudentController {
 
     @ApiOperation(value = "保存并更新某个学生的信息", tags = "学生类")
     @PostMapping("/update")
-    public void updateStudent(@RequestBody Student student){
+    public String updateStudent(@RequestBody Student student){
+        if(!service1.isStudentExist(student.getStudentId())){
+            return "学生不存在";
+        }
         service1.saveOrUpdate(student);
+        return "更新成功";
     }
     @ApiOperation(value = "返回某个学生的信息", tags = "学生类")
     @RequestMapping("/list")
     public String list(@RequestParam int StudentID){
+        if (!service1.isStudentExist(StudentID)){
+            return "学生不存在";
+        }
         Student student=service1.selectStudent(StudentID);
         Account account=service2.selectAccount(student.getAccountId());
         Account account1=new Account();
