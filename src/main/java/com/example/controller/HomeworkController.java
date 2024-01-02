bfb0be003178476c5eb0e338e7f604f7e1855aa4
Haoyu Li <lzoi_lhy@163.com>
2024-01-02 22:06:16 +0800

    listDDL2

--- src/main/java/com/example/controller/HomeworkController.java
+++ src/main/java/com/example/controller/HomeworkController.java
@@ -82,6 +82,50 @@ public class HomeworkController {
         }
         return JSON.toJSONString(ret);
     }
+    @PostMapping("/listDDL2")
+    public String listDDL2(@RequestParam Integer studentId){
+        LocalDateTime localDateTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String currentTime = localDateTime.format(formatter);
+        System.out.println(currentTime);
+        RelationshipCourse relationshipCourse = new RelationshipCourse();
+        relationshipCourse.setStudentId(studentId);
+        List<RelationshipCourse> listRelationshipCourse = relationshipCourseService.selectList(relationshipCourse);
+        Map<Integer, Integer> map2 = new HashMap<>();
+        List<Homework> ret = new ArrayList<>();
+        for (RelationshipCourse relationshipCourse1:listRelationshipCourse){
+            Integer courseId = relationshipCourse1.getCourseId();
+            if (map2.get(courseId)!=null) continue;
+            map2.put(courseId, 1);
+            Homework homework = new Homework();
+            homework.setClassId(courseId);
+            List<Homework> listHomework = service.selectList(homework);
+            for (Homework homework1 : listHomework){
+                if (homework1.getHomeworkDdl().compareTo(currentTime)<0){
+                    ret.add(homework1);
+                }
+            }
+        }
+        RelationshipStudentClassGroup relationshipStudentClassGroup = new RelationshipStudentClassGroup();
+        relationshipStudentClassGroup.setStudentId(studentId);
+        List<RelationshipStudentClassGroup> listRelationshipStudentClassGroup = relationshipStudentClassGroupService.selectList(relationshipStudentClassGroup);
+        for (RelationshipStudentClassGroup relationshipStudentClassGroup1:listRelationshipStudentClassGroup){
+            Integer groupId = relationshipStudentClassGroup1.getGroupId();
+            Group group = groupService.selectList(groupId);
+            Integer classId = group.getClassId();
+            if (map2.get(classId)!=null) continue;
+            map2.put(classId, 1);
+            Homework homework = new Homework();
+            homework.setClassId(classId);
+            List<Homework> listHomework = service.selectList(homework);
+            for (Homework homework1 : listHomework){
+                if (homework1.getHomeworkDdl().compareTo(currentTime)<0){
+                    ret.add(homework1);
+                }
+            }
+        }
+        return JSON.toJSONString(ret);
+    }
     @RequestMapping("/all")
     public String all() {
         return JSON.toJSONString(service.selectList(new Homework()));
