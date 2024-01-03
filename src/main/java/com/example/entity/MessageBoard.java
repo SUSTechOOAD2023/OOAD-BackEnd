26a48275b603c18c66741a6a31223eeb304f54df
SendingA <12110813@mail.sustech.edu.cn>
2024-01-03 16:47:59 +0800

    update messageBoard

--- src/main/java/com/example/entity/MessageBoard.java
+++ src/main/java/com/example/entity/MessageBoard.java
@@ -1,8 +1,14 @@
 package com.example.entity;
 
 import java.io.Serializable;
+import java.sql.Timestamp;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 /**
  * <p>
@@ -13,16 +19,20 @@ import lombok.EqualsAndHashCode;
  * @since 2024-01-03
  */
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @EqualsAndHashCode(callSuper = false)
 public class MessageBoard implements Serializable {
 
     private static final long serialVersionUID = 1L;
-
-      private Integer messageId;
+    @TableId(value = "message_id", type = IdType.AUTO)
+    private Integer messageId;
 
     private Integer studentId;
 
     private String message;
+
+    private Timestamp messageTime;
 
 
 }
