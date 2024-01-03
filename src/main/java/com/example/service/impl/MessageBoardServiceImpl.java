70ab8548a34523e299539a8d7194be678591289b
Haoyu Li <lzoi_lhy@163.com>
2024-01-03 16:47:53 +0800

    service

--- src/main/java/com/example/service/impl/MessageBoardServiceImpl.java
+++ src/main/java/com/example/service/impl/MessageBoardServiceImpl.java
@@ -1,10 +1,16 @@
 package com.example.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.example.entity.MessageBoard;
+import com.example.entity.Notice;
 import com.example.mapper.MessageBoardMapper;
+import com.example.mapper.NoticeMapper;
 import com.example.service.MessageBoardService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
@@ -16,5 +22,18 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class MessageBoardServiceImpl extends ServiceImpl<MessageBoardMapper, MessageBoard> implements MessageBoardService {
-
+    @Autowired
+    MessageBoardMapper mapper;
+    @Override
+    public List<MessageBoard> selectList(MessageBoard messageBoard){
+        QueryWrapper<MessageBoard> queryWrapper=new QueryWrapper<>();
+        queryWrapper.lambda().eq(messageBoard.getMessageId()!=null,MessageBoard::getMessageId,messageBoard.getMessageId());
+        return mapper.selectList(queryWrapper);
+    }
+    @Override
+    public int delete(MessageBoard messageBoard){
+        QueryWrapper<MessageBoard> queryWrapper=new QueryWrapper<>();
+        queryWrapper.lambda().eq(messageBoard.getMessageId()!=null,MessageBoard::getMessageId,messageBoard.getMessageId());
+        return mapper.delete(queryWrapper);
+    }
 }
